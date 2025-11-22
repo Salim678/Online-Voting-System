a51dad4208a04f537eb34b8f0dbb59c6a86fa6f4
Salim678 <mohammad.24scse1010678@galgotiasuniversity.ac.in>
2025-11-22 20:19:35 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+import service.AuthService;
+import model.Voter;
+
+public class Main {
+    public static void main(String[] args) {
+        AuthService auth = new AuthService();
+
+        // Sample registration test
+        Voter v = new Voter(0, "Test User", "test@gmail.com", "1234", false);
+        boolean result = auth.register(v);
+
+        if (result)
+            System.out.println("User Registered Successfully");
+        else
+            System.out.println("Registration Failed");
+    }
+}
