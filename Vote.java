a681e0441ca62dfa1624e1cbbb01a568cf266bad
Salim678 <mohammad.24scse1010678@galgotiasuniversity.ac.in>
2025-11-22 20:07:07 +0530

    Update Vote.java

--- Vote.java
+++ Vote.java
@@ -20,14 +20,34 @@ public class Vote {
         this.castAt = castAt;
     }
 
-    public int getVoteId() { return voteId; }
-    public void setVoteId(int voteId) { this.voteId = voteId; }
-    public int getVoterId() { return voterId; }
-    public void setVoterId(int voterId) { this.voterId = voterId; }
-    public int getCandidateId() { return candidateId; }
-    public void setCandidateId(int candidateId) { this.candidateId = candidateId; }
-    public int getElectionId() { return electionId; }
-    public void setElectionId(int electionId) { this.electionId = electionId; }
-    public java.sql.Timestamp getCastAt() { return castAt; }
-    public void setCastAt(java.sql.Timestamp castAt) { this.castAt = castAt; }
+    public int getVoteId() { 
+        return voteId; 
+    }
+    public void setVoteId(int voteId) {
+        this.voteId = voteId;
+    }
+    public int getVoterId() {
+        return voterId; 
+    }
+    public void setVoterId(int voterId) {
+        this.voterId = voterId;
+    }
+    public int getCandidateId() {
+        return candidateId; 
+    }
+    public void setCandidateId(int candidateId) {
+        this.candidateId = candidateId;
+    }
+    public int getElectionId() {
+        return electionId;
+    }
+    public void setElectionId(int electionId) {
+        this.electionId = electionId; 
+    }
+    public java.sql.Timestamp getCastAt() {
+        return castAt; 
+    }
+    public void setCastAt(java.sql.Timestamp castAt) { 
+        this.castAt = castAt;
+    }
 }
