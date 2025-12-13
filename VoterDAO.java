565c30ca2605ddd4ef074710a6628cbed9acd115
Salim678 <mohammad.24scse1010678@galgotiasuniversity.ac.in>
2025-12-13 21:58:04 +0530

    Update VoterDAO.java

--- VoterDAO.java
+++ VoterDAO.java
@@ -1,31 +1,78 @@
-
 package dao;
-
-import com.voting.model.Voter;
-import com.voting.util.DBConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import model.Voter;
+import util.DBConnection;
+
 public class VoterDAO {
 
-    public Voter login(String email, String password) {
-        String sql = "SELECT * FROM voters WHERE email=? AND password=?";
+    public static boolean isEmailExists(String email) {
+
+        String sql = "SELECT voter_id FROM voters WHERE email = ?";
+
         try (Connection con = DBConnection.getConnection();
              PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+
+            return rs.next();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return true;
+        }
+    }
+
+    public static boolean registerVoter(Voter voter) {
+
+        String sql = """
+            INSERT INTO voters(name, email, password, role, has_voted)
+            VALUES (?, ?, SHA2(?,256), 'voter', 0)
+        """;
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, voter.getName());
+            ps.setString(2, voter.getEmail());
+            ps.setString(3, voter.getPassword());
+
+            return ps.executeUpdate() > 0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static Voter validateLogin(String email, String password) {
+
+        String sql = """
+            SELECT voter_id, name, role
+            FROM voters
+            WHERE email = ? AND password = SHA2(?,256)
+        """;
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
             ps.setString(1, email);
             ps.setString(2, password);
-            try (ResultSet rs = ps.executeQuery()) {
-                if (rs.next()) {
-                    Voter v = new Voter();
-                    v.setVoterId(rs.getInt("voter_id"));
-                    v.setName(rs.getString("name"));
-                    v.setEmail(rs.getString("email"));
-                    v.setPassword(rs.getString("password"));
-                    return v;
-                }
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                Voter v = new Voter();
+                v.setId(rs.getInt("voter_id"));
+                v.setName(rs.getString("name"));
+                v.setRole(rs.getString("role"));
+                return v;
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
